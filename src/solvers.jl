b3c70bd0fc2dc7a74d071ad395af19d9f185efcc
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2022-06-18 09:12:34 +0200

    Try softmax with regularization instead of regularization

--- src/solvers.jl
+++ src/solvers.jl
@@ -7,16 +7,16 @@ end
 Base.showerror(io::IO, x::NonOptimalStatusError) =
     print(io, "NonOptimalStatusError(", x.termination_status, ")")
 
-function solve_least_squares(M::Matrix, q::Vector{Float64}, strategy::Symbol)
+function solve_least_squares(M::Matrix, q::Vector{Float64}, strategy::Symbol, λ::Float64=1e-6)
     model = Model(Ipopt.Optimizer)
     set_silent(model)
     C, F = size(M) # the numbers of classes and features
 
     # least squares ||q - M*p||_2^2
     if strategy == :softmax
-        @variable(model, -1 ≤ l[1:C] ≤ 1) # latent variables (unconstrained sum)
+        @variable(model, l[1:C]) # latent variables (unconstrained)
         @NLexpression(model, p[i = 1:C], exp(l[i]) / sum(exp(l[j]) for j in 1:C)) # p = softmax(l)
-        @NLobjective(model, Min, sum((q[i] - sum(M[i, j] * p[j] for j in 1:C))^2 for i in 1:F))
+        @NLobjective(model, Min, sum((q[i] - sum(M[i, j] * p[j] for j in 1:C))^2 for i in 1:F) + λ * sum(l[j]^2 for j in 1:C))
     elseif strategy == :constrained
         @variable(model, p[1:C] ≥ 0) # p_i ≥ 0
         @constraint(model, ones(C)' * p == 1) # 1' * p = 1
