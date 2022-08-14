f018b4a1dc0ceb373f26d5f4ffdddb3e7fe6a1ba
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2022-08-14 10:04:37 +0200

    Move crab.mk to crab_comparison.mk

--- experiments/crab_comparison.mk
+++ experiments/crab_comparison.mk
@@ -8,4 +8,7 @@ test: results/test_crab_comparison_10k.tex
 results/test_crab_comparison_10k.tex: crab_comparison.jl
 	$(_JULIA) $< --is_test_run --test_path results/test_crab_comparison_test.csv --validation_path results/test_crab_comparison_validation.csv results/test_crab_comparison_01k.tex $@
 
+results/crab_protocols_nmd.tex: crab_protocols.jl
+	julia --project=. $^ results/crab_protocols_Tp.tex $@
+
 .PHONY: experiment test
