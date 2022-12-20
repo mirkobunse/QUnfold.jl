e33dcdd5d344fa7e6ce1c2ebc4f23a3d5b405395
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2022-12-20 15:14:45 +0100

    Fix np.float bug in QuaPy

--- experiments/docker/setup.jl
+++ experiments/docker/setup.jl
@@ -6,4 +6,8 @@ pkg"precompile"
 
 # fix Python package versions for reproducibility
 using PyCall
-pyimport("pip").main(["install", "scikit-learn==1.1.1", "quapy==0.1.6"])
+pyimport("pip").main(["install",
+    "scikit-learn==1.1.1",
+    "quapy==0.1.6",
+    "numpy==1.23.3" # 1.24 errors with QuaPy using np.float
+    ])
