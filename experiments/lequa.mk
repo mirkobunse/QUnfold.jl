0c2ca6c91b5aa0932de2d645865b4e919e780be0
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2023-01-02 11:26:19 +0100

    Makefile target dev executes the new :dev configuration

--- experiments/lequa.mk
+++ experiments/lequa.mk
@@ -1,5 +1,6 @@
-_TABLES=results/lequa.tex results/lequa_validation.tex
-_EXPERIMENTS=results/lequa.csv results/lequa_validation.csv
+_TABLES=results/lequa_lq22.tex results/lequa_lq22_validation.tex
+_EXPERIMENTS=results/lequa_lq22.csv results/lequa_lq22_validation.csv
+_DEV=results/lequa_dev.tex results/lequa_dev_validation.tex
 _TESTS=results/test_lequa.tex
 _DATA=data/T1B/public/training_data.txt data/T1B/public/test_prevalences.txt data/T1B/public/test_samples/4999.txt
 _TRAINING_URL=https://zenodo.org/record/6546188/files/T1B.train_dev.zip?download=1
@@ -7,16 +8,26 @@ _PREVALENCES_URL=https://zenodo.org/record/6546188/files/T1B.test_prevalences.zi
 _TEST_URL=https://zenodo.org/record/6546188/files/T1B.test.zip?download=1
 
 tables: $(_TABLES)
-results/lequa.tex: generate_lequa_tables.jl results/lequa_validation.csv results/lequa.csv
+results/lequa_lq22.tex: generate_lequa_tables.jl results/lequa_lq22_validation.csv results/lequa_lq22.csv
 	julia --project=. $^ $@
-results/lequa_validation.tex: generate_lequa_tables.jl results/lequa_validation.csv
-	julia --project=. $^ results/lequa_validation.csv $@
+results/lequa_lq22_validation.tex: generate_lequa_tables.jl results/lequa_lq22_validation.csv
+	julia --project=. $^ results/lequa_lq22_validation.csv $@
 
 experiments: $(_EXPERIMENTS)
-results/lequa.csv: lequa.jl $(_DATA) $(shell find ../src -name "*.jl")
+results/lequa_lq22.csv: lequa.jl $(_DATA) $(shell find ../src -name "*.jl")
 	julia --project=. $< $@
-results/lequa_validation.csv: lequa.jl $(_DATA) $(shell find ../src -name "*.jl")
+results/lequa_lq22_validation.csv: lequa.jl $(_DATA) $(shell find ../src -name "*.jl")
 	julia --project=. $< --is_validation_run $@
+
+dev: $(_DEV)
+results/lequa_dev.tex: generate_lequa_tables.jl results/lequa_dev_validation.csv results/lequa_dev.csv
+	julia --project=. $^ $@
+results/lequa_dev_validation.tex: generate_lequa_tables.jl results/lequa_dev_validation.csv
+	julia --project=. $^ results/lequa_dev_validation.csv $@
+results/lequa_dev.csv: lequa.jl $(_DATA) $(shell find ../src -name "*.jl")
+	julia --project=. $< --configuration dev $@
+results/lequa_dev_validation.csv: lequa.jl $(_DATA) $(shell find ../src -name "*.jl")
+	julia --project=. $< --configuration dev --is_validation_run $@
 
 tests: $(_TESTS)
 results/test_lequa.tex: generate_lequa_tables.jl results/test_lequa_validation.csv results/test_lequa.csv
@@ -40,4 +51,4 @@ data/test_prevalences.zip:
 data/test.zip:
 	curl --fail --create-dirs --output $@ $(_TEST_URL)
 
-.PHONY: tables experiments tests data
+.PHONY: tables dev experiments tests data
